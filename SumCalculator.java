5c5b207ca81869d8bb2ff6fc6bdbdd626a5ecdda
Thabhashwinni-0128 <kthabhashwinni43@gmail.com>
2025-03-13 17:44:08 +0530

    Update SumCalculator.java

--- SumCalculator.java
+++ SumCalculator.java
@@ -8,7 +8,7 @@ public class Sumcalculator {
         double num1 = scanner.nextDouble();
 
         System.out.print("Enter Second number: ");
-        double num2 = scanner.nextDouble();
+        double num4 = scanner.nextDouble();
 
         double sum = num1 + num2;
 
